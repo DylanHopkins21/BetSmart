10b2fc7d135e5eb78f5fd15960a68cbb00e59267
audreysunzhang <audreyszhang@berkeley.edu>
2024-12-04 02:10:50 -0800

    history done

--- betsmart-frontend/src/pages/history/index.tsx
+++ betsmart-frontend/src/pages/history/index.tsx
@@ -3,8 +3,13 @@ import Link from "next/link";
 import "./history.css";
 import Wager from "@/components/wager/wager";
 import monkeyImage from './images/monkeyimage.jpg'; 
+import { CiWallet } from "react-icons/ci";
 
-const History = () => {
+interface HistoryProps {
+  earnings: number; 
+}
+
+export function History({ earnings = 50 }: HistoryProps) {
   return (
     <div className="history-container">
       <div className="history-header">
@@ -20,11 +25,44 @@ const History = () => {
       </div>
 
       <div className = "totEarnings">
-        
+        <CiWallet className = "wallet" />
+        MY TOTAL EARNINGS
+        <div className = "earningNumBox">
+          <p>${earnings}</p>
+        </div>
       </div>
 
       <div className = "pastWagers">
-        
+        <div className = "pastWagersTitle">
+          $ PAST WAGERS
+        </div>
+        <Wager
+              name="CS61B Midterm 2"
+              semester="Fall 2021"
+              image="/images/longimage.jpg"
+              entry={5}
+              partNum={7}
+              timeLeft={60}
+              win = {false}
+        />
+        <Wager
+              name="Poo Poo Decal"
+              semester="Fall 2030"
+              image="/images/longimage2.jpg"
+              entry={10}
+              partNum={10}
+              timeLeft={100}
+              win = {true}
+        />
+        <Wager
+              name="CS 70 Final"
+              semester="Fall 2022"
+              image="/images/monkeyimage.jpg"
+              entry={12}
+              partNum={48}
+              timeLeft={200}
+              win = {false}
+        />
       </div>
     </div>
   );
