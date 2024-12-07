b253d90e391893662e84514dfa57882abf894180
siyamehta12 <siyamehta@berkeley.edu>
2024-12-07 15:22:16 -0800

    wager

--- betsmart-frontend/src/pages/index.tsx
+++ betsmart-frontend/src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetServerSideProps } from "next";
+import Head from "next/head";
+import React, { useState } from "react";
+import Modal from "../components/wagerModal/wagerModal";
 
 export const getServerSideProps: GetServerSideProps = async () => {
   return {
@@ -10,5 +13,25 @@ export const getServerSideProps: GetServerSideProps = async () => {
 };
 
 export default function Home() {
-  return null; 
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <div className="style1">
+        <h1 id="test">BetSmart</h1>
+        <h3>Bet on Grades</h3>
+        <p>Made by: Siya, Audrey, Dylan, and Mohammed</p>
+        <main>
+      <button onClick={() => setIsOpen(true)}>
+        Open Modal
+      </button>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
+    </main>
+      </div>
+    </>
+  );
 }
+
+
