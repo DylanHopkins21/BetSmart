3f2c7876acefc183696d405cb743bf9259386a8c
audreysunzhang <audreyszhang@berkeley.edu>
2024-12-06 18:46:32 -0800

    push for mohammed

--- betsmart-backend/api.py
+++ betsmart-backend/api.py
@@ -1,16 +1,30 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/poop/<name>', methods = ['GET'])
-def hello(name):
-    return f'hi {name}'
+#login
+@app.route('/gradescopeAuth')
 
-@app.route("/get_users")
-def user_json():
-    return {
-        "username": "dhoppers",
-        "password": "pass"
-    }
+@app.route('/getActiveWagers')
+def getActiveWagers():
+    return {"name" : "audrey"}
+
+@app.route('/getNotifs')
+
+@app.route('/expireWager')
+#move when some negative time 
+
+@app.route('/getPastWagers')
+
+@app.route('/getTotalEarnings')
+#summation of all entries in past wagers 
+
+
+@app.route('/acceptInvite')
+
+@app.route('/rejectInvite')
+
+@app.route('/createWager')
+#move created to add to active wagers 
 
 if __name__ == "__main__":
     app.run()
