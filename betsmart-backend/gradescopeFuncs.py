36935ca97bcca5347806e8738dbc016c12955ed9
Mohammed Amin <coldprohor12@gmail.com>
2024-12-07 05:01:49 -0800

    cleaned up gradescope funcs exmaple

--- betsmart-backend/gradescopeFuncs.py
+++ betsmart-backend/gradescopeFuncs.py
@@ -4,26 +4,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 password = os.getenv("PASSWORD")
+
 connection = GSConnection()
 connection.login("mohammedamin@berkeley.edu", password)
 courses = connection.account.get_courses()
 
 print("Courses:")
-course_name = None 
+course_name = None
+
 for role, role_courses in courses.items():
     print(f"\n{role.capitalize()} Courses:")
     for course_id, course in role_courses.items():
         print(f"Course ID: {course_id}, Course Name: {course.name}")
-        if course_id == 719091: 
+        if int(course_id) == 719091:
             course_name = course.name
+
 if course_name:
     print(f"\nAssignments for Course: {course_name}")
 else:
     print("\nAssignments for Course ID 719091 (Course Name not found)")
 
-course_id = 719091 
+course_id = 719091  
 assignments = connection.account.get_assignments(course_id)
 
+# Print assignments
 for assignment in assignments:
     print(f"Assignment: {assignment.name}")
     print(f"Grade: {assignment.grade}/{assignment.max_grade}")
